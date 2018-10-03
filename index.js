eb4b7066d0d2089be7686b8df29998b545773409
Sinan Balçın <sinan.balcin@hititdc.com>
2018-10-03 10:27:14 +0300

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 // Import required bot services. See https://aka.ms/bot-services to learn more about the different part of a bot
 const { BotFrameworkAdapter, MemoryStorage, ConversationState, UserState } = require('botbuilder');
-const { BotConfiguration } = require('botframework-config');
 
 // Import our custom bot class that provides a turn handling function.
 const { SimplePromptBot } = require('./bot');
@@ -24,33 +23,18 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
     console.log(`\nTo talk to your bot, open simple-prompt-bot.bot file in the Emulator`);
 });
 
-// .bot file path
-const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
 
-let botConfig;
-try {
-    // Read bot configuration from .bot file.
-    botConfig = BotConfiguration.loadSync(BOT_FILE, process.env.botFileSecret);
-} catch (err) {
-    console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
-    console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
-    console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.\n\n`);
-    process.exit();
-}
-
-const DEV_ENVIRONMENT = 'development';
 
 // bot name as defined in .bot file
 // See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
-const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
 
 // Get bot endpoint configuration by service name
-const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
 
 // Create adapter. See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration .
+
 const adapter = new BotFrameworkAdapter({
-    appId: endpointConfig.appId || process.env.microsoftAppID,
-    appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
+    appId: '4129deeb-9681-4178-b23a-dde4b6e4d393',
+    appPassword: '2pe1ld0/gOBOoxAukIEt9PwiZKtRJpSYvuJZazQnniQ='
 });
 
 // Define state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
