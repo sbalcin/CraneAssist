b424d4c8c6610380dfb309edaa6516f27f490433
Sinan Balçın <sinan.balcin@hititdc.com>
2018-10-03 14:53:25 +0300

    id pass update 17

--- bot.js
+++ bot.js
@@ -1,110 +1,127 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// Import required Bot Framework classes.
 const { ActivityTypes } = require('botbuilder');
-const { CardFactory } = require('botbuilder');
+const { TextPrompt, DialogSet, WaterfallDialog } = require('botbuilder-dialogs');
 
-// Adaptive Card content
-const IntroCard = require('./resources/IntroCard.json');
+const DIALOG_STATE_PROPERTY = 'dialogState';
+const USER_NAME_PROP = 'user_name';
+const WHO_ARE_YOU = 'who_are_you';
+const HELLO_USER = 'hello_user';
 
-// Welcomed User property name
-const WELCOMED_USER = 'welcomedUserProperty';
+const NAME_PROMPT = 'name_prompt';
 
-class WelcomeBot {
+class SimplePromptBot {
     /**
      *
-     * @param {UserState} User state to persist boolean flag to indicate
-     *                    if the bot had already welcomed the user
+     * @param {Object} conversationState
+     * @param {Object} userState
      */
-    constructor(userState) {
-        // Creates a new user property accessor.
-        // See https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors.
-        this.welcomedUserProperty = userState.createProperty(WELCOMED_USER);
-
+    constructor(conversationState, userState) {
+        // creates a new state accessor property. see https://aka.ms/about-bot-state-accessors to learn more about the bot state and state accessors
+        this.conversationState = conversationState;
         this.userState = userState;
+
+        this.dialogState = this.conversationState.createProperty(DIALOG_STATE_PROPERTY);
+
+        this.userName = this.userState.createProperty(USER_NAME_PROP);
+
+        this.dialogs = new DialogSet(this.dialogState);
+
+        // Add prompts
+        this.dialogs.add(new TextPrompt(NAME_PROMPT));
+
+        // Create a dialog that asks the user for their name.
+        this.dialogs.add(new WaterfallDialog(WHO_ARE_YOU, [
+            this.askForName.bind(this),
+            this.collectAndDisplayName.bind(this)
+        ]));
+
+        // Create a dialog that displays a user name after it has been collceted.
+        this.dialogs.add(new WaterfallDialog(HELLO_USER, [
+            this.displayName.bind(this)
+        ]));
     }
+
+    // The first step in this waterfall asks the user for their name.
+    async askForName(dc, step) {
+        return await dc.prompt(NAME_PROMPT, `What is your name, human?`);
+    }
+
+    // The second step in this waterfall collects the response, stores it in
+    // the state accessor, then displays it.
+    async collectAndDisplayName(step) {
+        await this.userName.set(step.context, step.result);
+        await step.context.sendActivity(`Got it. You are ${ step.result }.`);
+        return await step.endDialog();
+    }
+
+    // This step loads the user's name from state and displays it.
+    async displayName(step) {
+        const userName = await this.userName.get(step.context, null);
+        await step.context.sendActivity(`Your name is ${ userName }.`);
+        return await step.endDialog();
+    }
+
     /**
      *
-     * @param {TurnContext} context on turn context object.
+     * @param {Object} context on turn context object.
      */
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
-            // Read UserState. If the 'DidBotWelcomedUser' does not exist (first time ever for a user)
-            // set the default to false.
-            let didBotWelcomedUser = await this.welcomedUserProperty.get(turnContext, false);
+            // Create dialog context
+            const dc = await this.dialogs.createContext(turnContext);
 
-            // Your bot should proactively send a welcome message to a personal chat the first time
-            // (and only the first time) a user initiates a personal chat with your bot.
-            if (didBotWelcomedUser === false) {
-                // The channel should send the user name in the 'From' object
-                let userName = turnContext.activity.from.name;
-                await turnContext.sendActivity('You are seeing this message because this was your first message ever sent to this bot.');
-                await turnContext.sendActivity(`It is a good practice to welcome the user and provide personal greeting. For example, welcome ${ userName }.`);
-
-                // Set the flag indicating the bot handled the user's first message.
-                await this.welcomedUserProperty.set(turnContext, true);
-            } else {
-                // This example uses an exact match on user's input utterance.
-                // Consider using LUIS or QnA for Natural Language Processing.
-                let text = turnContext.activity.text.toLowerCase();
-                switch (text) {
-                case 'hello':
-                case 'hi':
-                    await turnContext.sendActivity(`You said "${ turnContext.activity.text }"`);
-                    break;
-                case 'intro':
-                case 'help':
-                    await turnContext.sendActivity({
-                        text: 'Intro Adaptive Card',
-                        attachments: [CardFactory.adaptiveCard(IntroCard)]
-                    });
-                    break;
-                default :
-                    await turnContext.sendActivity(`This is a simple Welcome Bot sample. You can say 'intro' to
-                                                        see the introduction card. If you are running this bot in the Bot
-                                                        Framework Emulator, press the 'Start Over' button to simulate user joining a bot or a channel`);
+            const utterance = (turnContext.activity.text || '').trim().toLowerCase();
+            if (utterance === 'cancel') {
+                if (dc.activeDialog) {
+                    await dc.cancelAllDialogs();
+                    await dc.context.sendActivity(`Ok... Cancelled.`);
+                } else {
+                    await dc.context.sendActivity(`Nothing to cancel.`);
                 }
             }
-            // Save state changes
-            await this.userState.saveChanges(turnContext);
-        } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate) {
-            // Send greeting when users are added to the conversation.
-            await this.sendWelcomeMessage(turnContext);
-        } else {
-            // Generic message for all other activities
-            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
-        }
-    }
 
-    /**
-     * Sends welcome messages to conversation members when they join the conversation.
-     * Messages are only sent to conversation members who aren't the bot.
-     * @param {TurnContext} turnContext
-     */
-    async sendWelcomeMessage(turnContext) {
-        // Do we have any new members added to the conversation?
-        if (turnContext.activity.membersAdded.length !== 0) {
-            // Iterate over all new members added to the conversation
-            for (var idx in turnContext.activity.membersAdded) {
-                // Greet anyone that was not the target (recipient) of this message.
-                // Since the bot is the recipient for events from the channel,
-                // context.activity.membersAdded === context.activity.recipient.Id indicates the
-                // bot was added to the conversation, and the opposite indicates this is a user.
-                if (turnContext.activity.membersAdded[idx].id !== turnContext.activity.recipient.id) {
-                    await turnContext.sendActivity(`Welcome to the 'Welcome User' Bot. This bot will introduce you to welcoming and greeting users.`);
-                    await turnContext.sendActivity("You are seeing this message because the bot recieved atleast one 'ConversationUpdate'" +
-                                            'event,indicating you (and possibly others) joined the conversation. If you are using the emulator, ' +
-                                            "pressing the 'Start Over' button to trigger this event again. The specifics of the 'ConversationUpdate' " +
-                                            'event depends on the channel. You can read more information at https://aka.ms/about-botframework-welcome-user');
-                    await turnContext.sendActivity(`It is a good pattern to use this event to send general greeting to user, explaining what your bot can do. ` +
-                                            `In this example, the bot handles 'hello', 'hi', 'help' and 'intro. ` +
-                                            `Try it now, type 'hi'`);
+            // Continue the current dialog
+            if (!turnContext.responded) {
+                await dc.continueDialog();
+            }
+
+            // Show menu if no response sent
+            if (!turnContext.responded) {
+                var userName = await this.userName.get(dc.context, null);
+                if (userName) {
+                    await dc.beginDialog(HELLO_USER);
+                } else {
+                    await dc.beginDialog(WHO_ARE_YOU);
+                }
+            }
+        } else if (
+            turnContext.activity.type === ActivityTypes.ConversationUpdate
+        ) {
+            // Do we have any new members added to the conversation?
+            if (turnContext.activity.membersAdded.length !== 0) {
+                // Iterate over all new members added to the conversation
+                for (var idx in turnContext.activity.membersAdded) {
+                    // Greet anyone that was not the target (recipient) of this message.
+                    // Since the bot is the recipient for events from the channel,
+                    // context.activity.membersAdded === context.activity.recipient.Id indicates the
+                    // bot was added to the conversation, and the opposite indicates this is a user.
+                    if (turnContext.activity.membersAdded[idx].id !== turnContext.activity.recipient.id) {
+                        // Send a "this is what the bot does" message to this user.
+                        await turnContext.sendActivity('I am a bot that demonstrates the TextPrompt class to collect your name, store it in UserState, and display it. Say anything to continue.');
+                    }
                 }
             }
         }
+
+        // Save changes to the user name.
+        await this.userState.saveChanges(turnContext);
+
+        // End this turn by saving changes to the conversation state.
+        await this.conversationState.saveChanges(turnContext);
     }
 }
 
-module.exports.WelcomeBot = WelcomeBot;
+module.exports.SimplePromptBot = SimplePromptBot;
--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// Import required packages
-const path = require('path');
 const restify = require('restify');
+const path = require('path');
 
-// Import required bot services. See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { BotFrameworkAdapter, ConversationState, MemoryStorage } = require('botbuilder');
-// Import required bot configuration.
+// Import required bot services. See https://aka.ms/bot-services to learn more about the different part of a bot
+const { BotFrameworkAdapter, MemoryStorage, ConversationState, UserState } = require('botbuilder');
 const { BotConfiguration } = require('botframework-config');
 
-const { WelcomeBot } = require('./bot');
+// Import our custom bot class that provides a turn handling function.
+const { SimplePromptBot } = require('./bot');
 
 // Read botFilePath and botFileSecret from .env file
 // Note: Ensure you have a .env file and include botFilePath and botFileSecret.
 const ENV_FILE = path.join(__dirname, '.env');
-const env = require('dotenv').config({ path: ENV_FILE });
+require('dotenv').config({ path: ENV_FILE });
 
-// Get the .bot file path
-// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
+// Create HTTP server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo talk to your bot, open simple-prompt-bot.bot file in the Emulator`);
+});
+
+// .bot file path
 const BOT_FILE = path.join(__dirname, (process.env.botFilePath || ''));
+
 let botConfig;
 try {
     // Read bot configuration from .bot file.
@@ -27,49 +34,28 @@ try {
 } catch (err) {
     console.error(`\nError reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.`);
     console.error(`\n - The botFileSecret is available under appsettings for your Azure Bot Service bot.`);
-    console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.`);
-    console.error(`\n - See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.\n\n`);
+    console.error(`\n - If you are running this bot locally, consider adding a .env file with botFilePath and botFileSecret.\n\n`);
     process.exit();
 }
 
-// For local development configuration as defined in .bot file
 const DEV_ENVIRONMENT = 'development';
 
-// Define name of the endpoint configuration section from the .bot file
+// bot name as defined in .bot file
+// See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
 const BOT_CONFIGURATION = (process.env.NODE_ENV || DEV_ENVIRONMENT);
 
 // Get bot endpoint configuration by service name
-// Bot configuration as defined in .bot file
 const endpointConfig = botConfig.findServiceByNameOrId(BOT_CONFIGURATION);
 
-// Create bot adapter.
-// See https://aka.ms/about-bot-adapter to learn more about bot adapter.
+// Create adapter. See https://aka.ms/about-bot-adapter to learn more about .bot file its use and bot configuration .
 const adapter = new BotFrameworkAdapter({
     appId: endpointConfig.appId || process.env.microsoftAppID,
     appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
 });
 
-// Catch-all for any unhandled errors in your bot.
-adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    console.error(`\n [onTurnError]: ${ error }`);
-    // Send a message to the user
-    context.sendActivity(`Oops. Something went wrong!`);
-    // Clear out state
-    await conversationState.clear(context);
-    // Save state changes.
-    await conversationState.saveChanges(context);
-};
-
-// Define a state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
-// A bot requires a state store to persist the dialog and user state between messages.
-let conversationState;
-
-// For local development, in-memory storage is used.
-// CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
-// is restarted, anything stored in memory will be gone.
+// Define state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
+// A bot requires a some sort of state storage system to persist the dialog and user state between messages.
 const memoryStorage = new MemoryStorage();
-conversationState = new ConversationState(memoryStorage);
 
 // CAUTION: You must ensure your product environment has the NODE_ENV set
 //          to use the Azure Blob storage or Azure Cosmos DB providers.
@@ -84,23 +70,28 @@ conversationState = new ConversationState(memoryStorage);
 //     containerName: (blobStorageConfig.container || DEFAULT_BOT_CONTAINER),
 //     storageAccountOrConnectionString: blobStorageConfig.connectionString,
 // });
-// conversationState = new ConversationState(blobStorage);
 
-// Create the main dialog.
-const bot = new WelcomeBot(conversationState);
+// Create conversation state with in-memory storage provider.
+const conversationState = new ConversationState(memoryStorage);
+const userState = new UserState(memoryStorage);
 
-// Create HTTP server
-let server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function() {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
-    console.log(`\nTo talk to your bot, open echoBot-with-counter.bot file in the Emulator`);
-});
+// Create the bot object that provides the turn handler function.
+const bot = new SimplePromptBot(conversationState, userState);
 
-// Listen for incoming activities and route them to your bot main dialog.
+// Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
-        // route to main dialog.
+        // Route theincoming activity to the main bot turn handler.
         await bot.onTurn(context);
     });
 });
+
+// Catch-all for errors.
+adapter.onTurnError = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    console.error(`\n [onTurnError]: ${ error }`);
+    // Send a message to the user
+    context.sendActivity(`Oops. Something went wrong!`);
+    // Clear out state
+    conversationState.clear(context);
+};
